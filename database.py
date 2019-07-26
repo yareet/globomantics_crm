7ff2c1794ec91a1fd1d40a4db1215740c75543d0
nick <njrusmc@Gmail.com>
2019-07-26 15:45:49 -0400

    using USD format

--- database.py
+++ database.py
@@ -45,6 +45,7 @@ class Database:
         acct = self.data.get(acct_id)
         if acct:
             bal = float(acct["due"]) - float(acct["paid"])
-            return f"$ {bal:.2f}"
+            return f"{bal:.2f} USD"
+            # return f"$ {bal:.2f}"
             # return int(acct["due"]) - int(acct["paid"])
         return None
