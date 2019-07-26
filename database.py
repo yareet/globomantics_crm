cfc842ecb9dc144c6e93d15804716f324dadf1d4
nick <njrusmc@Gmail.com>
2019-07-26 07:50:31 -0400

    added decimal and $

--- database.py
+++ database.py
@@ -44,5 +44,7 @@ class Database:
         """
         acct = self.data.get(acct_id)
         if acct:
-            return int(acct["due"]) - int(acct["paid"])
+            bal = float(acct["due"]) - float(acct["paid"])
+            return f"$ {bal:.2f}"
+            # return int(acct["due"]) - int(acct["paid"])
         return None
